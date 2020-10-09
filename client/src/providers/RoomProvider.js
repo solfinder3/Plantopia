6e2b63223717df4127be0ffa01d9ce6308a1260b
Braxton McClellan <thebraxmc@gmail.com>
2020-10-08 19:56:43 -0600

    rooms.js

--- client/src/providers/RoomProvider.js
+++ client/src/providers/RoomProvider.js
@@ -5,13 +5,63 @@ export const RoomContext = React.createContext();
 
 export const RoomConsumer = RoomContext.Consumer;
 
-const RoomProvider = () => {
-  const [rooms, setRooms] = useState()
+const RoomProvider = ({children}) => {
+  const [rooms, setRooms] = useState([])
   const [room, setRoom] = useState()
 
   const getRooms = () => {
-    axios.get()
+    axios.get(`/api/rooms`)
+    .then( res => {
+      setRooms( res.data )
+    })
+    .catch(err => console.log(err))
   }
+
+  const addRoom = (room) => {
+    axios.post(`/api/rooms`, {room})
+    .then( res => {
+      setRooms([...rooms, res.data])
+    })
+    .catch(err => console.log(err))
+  }
+
+  const updateRoom = (id, room) => {
+    axios.put(`/api/rooms/${id}`, {room})
+    .then( res => {
+      const updatedRooms = rooms.map( r => {
+        if (r.id === id) {
+          return res.data
+        }
+        return r
+      })
+      setRooms(updatedRooms)
+    })
+    .catch(err => console.log(err))
+  }
+
+  const deleteRoom = (id) => {
+    axios.delete(`/api/rooms/${id}`)
+    .then( res => {
+      setRooms(rooms.filter(r => r.id !== id))
+      alert(res.data.message)
+    })
+    .catch(err => console.log(err))
+  }
+
+
+
+  return (
+    <RoomContext.Provider value={{
+      rooms: rooms,
+      room: room,
+      getRooms: getRooms,
+      addRoom: addRoom,
+      updateRoom: updateRoom,
+      deleteRoom: deleteRoom
+    }}>
+      { children }
+    </RoomContext.Provider>
+  )
 
 }
 
